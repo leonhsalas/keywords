073c103205ce74ee49fefb045aa47b8cbf0b8597
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-14 10:32:54 -0600

    martes

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 def cargakeywords():
-    pass
+    with open("keywords.txt") as f:
+        for linea in f:
+            keywords = linea
+            print(keywords)
 def finalizar():
-    pass
+    print("Project ended")
+
+
+numero = int(input("Cual es el numero "))
+
+while numero != 0:
+    if numero == 1:
+        cargakeywords()
+    elif numero == 0:
+        finalizar()
+        break
+    else:
+        print("pon 1. 2 o 0")
+    numero = int(input("Cual es el numero "))
+
+finalizar()
